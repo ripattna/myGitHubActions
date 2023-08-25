eb811dc8cb0e838f9430da5d176d49ceabe05d45
Rissan Pattnaik <ripattna@gmail.com>
2023-08-25 18:10:50 +0530

    New changes

---
+++ PythonTest.py
@@ -0,0 +1,10 @@
+"""This is a method to calculate"""
+def is_number_even(my_var):
+    """This is a functon to calculate event number"""
+    return "Even" if my_var % 2 == 0 else "Odd"
+
+my_variable = 6
+
+# my_var = input("Enter a number: ")
+
+print(f"The number {my_variable} is {is_number_even(my_variable)}")
