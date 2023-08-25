75c4c30bb9b2f1f2d2dddd56fd52513f66473918
Rissan Pattnaik <ripattna@gmail.com>
2023-08-25 18:33:26 +0530

    new changes

--- PythonTest.py
+++ PythonTest.py
@@ -1,8 +1,10 @@
 """ File contains various function to under Pylint """
 
+
 def is_number_even(num):
     """Function to check if number is even or odd"""
     return "Even" if num % 2 == 0 else "Odd"
+
 
 NUM = 5
 print(f"The number {NUM} is {is_number_even(NUM)}")
